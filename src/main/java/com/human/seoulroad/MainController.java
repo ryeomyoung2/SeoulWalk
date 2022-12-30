ff8210dfa02f6c1e75dd584e40d1225bc672dd0e
ryeomyoung2 <ryeomyoung2@gmail.com>
2022-12-30 11:41:59 +0900

    files:Updated

--- src/main/java/com/human/seoulroad/MainController.java
+++ src/main/java/com/human/seoulroad/MainController.java
@@ -29,6 +29,34 @@ public class MainController {
 	public String course1() {
 		return "course1";
 	}
+	@RequestMapping("/home/course/course2")
+	public String course2() {
+		return "course2";
+	}
+	@RequestMapping("/home/course/course3")
+	public String course3() {
+		return "course3";
+	}
+	@RequestMapping("/home/course/course4")
+	public String course4() {
+		return "course4";
+	}
+	@RequestMapping("/home/course/course5")
+	public String course5() {
+		return "course5";
+	}
+	@RequestMapping("/home/course/course6")
+	public String course6() {
+		return "course6";
+	}
+	@RequestMapping("/home/course/course7")
+	public String course7() {
+		return "course7";
+	}
+	@RequestMapping("/home/course/course8")
+	public String course8() {
+		return "course8";
+	}
 	@RequestMapping("/home/other")
 	public String other() {
 		return "other";
